c4238968d2e9a383007f8e55d52c67c67704b9e9
stxisonepur <114742675+stxisonepur@users.noreply.github.com>
2022-10-01 17:22:58 +0530

    Add files via upload

---
+++ result.php
@@ -0,0 +1,11 @@
+<?php
+    $name=$_POST['Name'];
+    $age=$_POST['Class'];
+    $city=$_POST['RN'];
+  
+    echo "NAME-SUBMITTED : $name <br>";
+  
+    echo "class-SUBMITTED :  $age <br>";
+  
+    echo "rolls-SUBMITTED:  $city";
+?>
